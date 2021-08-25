4b7b8fecc6ed3109d39d91aabdc3c43ffb4adb46
Di Xiao <mopjtv@gmail.com>
2021-08-25 16:31:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Deexiao />
+</template>
+
+<script>
+import Deexiao from './components/Deexiao.vue'
+export default {
+  name: 'App',
+  components: {
+    Deexiao,
+  },
+}
+</script>

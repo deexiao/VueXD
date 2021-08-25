ecdacfc724ae672ed6cc9281540a8a0ba74ed4a1
Di Xiao <mopjtv@gmail.com>
2021-08-25 19:53:17 +0800

    aside css 1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -10,7 +10,7 @@
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/swich">Switch 组件</router-link>
+            <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
           <li>
             <router-link to="/doc/button">Button 组件</router-link>
@@ -33,3 +33,22 @@ export default {
   components: { Topnav },
 }
 </script>
+
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>

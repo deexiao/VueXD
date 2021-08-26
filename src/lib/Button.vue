ab7c9eae04b5f3f4d17d59a46952d638207bca3b
Di Xiao <mopjtv@gmail.com>
2021-08-26 16:34:34 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -51,5 +51,23 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.VueXD-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.VueXD-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 </style>

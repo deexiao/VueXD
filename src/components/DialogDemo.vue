d61fab2bcb853bcb0f43bae9e9f9f5722679cc7f
Di Xiao <mopjtv@gmail.com>
2021-08-26 21:07:23 +0800

    Dialog css 1

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,21 @@
 <template>
   <h1>示例 1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 export default {
-  components: { Dialog },
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return { toggle, x }
+  },
 }
 </script>

adb379801629629d71dae7af8aeec800489e68fb
Di Xiao <mopjtv@gmail.com>
2021-08-27 18:42:49 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,23 @@
 <template>
-  <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="VueXD-tabs">
+    <div class="VueXD-tabs-nav">
+      <div
+        class="VueXD-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="VueXD-tabs-content">
+      <component
+        class="VueXD-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -24,4 +41,29 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.VueXD-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>

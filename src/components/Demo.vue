e1876447bc7676188d0893e69ab606a27185474c
Di Xiao <mopjtv@gmail.com>
2021-08-29 16:15:59 +0800

    update

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,53 +1,57 @@
 <template>
-<div class="demo">
-  <h2>{{component.__sourceCodeTitle}}</h2>
-  <div class="demo-component">
-    <component :is="component" />
+  <div class="demo">
+    <h2>{{ component.__sourceCodeTitle }}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
+    </div>
+    <div class="demo-code" v-if="codeVisible">
+      <pre class="language-html" v-html="html" />
+    </div>
   </div>
-  <div class="demo-actions">
-    <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
-    <Button @click="showCode" v-else>查看代码</Button>
-  </div>
-  <div class="demo-code" v-if="codeVisible">
-    <pre class="language-html" v-html="html" />
-  </div>
-
-</div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-import {
-  computed,
-  ref
-} from 'vue';
+import 'prismjs'
+// import 'prismjs/themes/prism.css'
+import { computed, ref } from 'vue'
 const Prism = (window as any).Prism
 export default {
   components: {
-    Button
+    Button,
   },
   props: {
-    component: Object
+    component: Object,
   },
   setup(props) {
     const html = computed(() => {
-      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
+      return Prism.highlight(
+        props.component.__sourceCode,
+        Prism.languages.html,
+        'html'
+      )
     })
-    const showCode = () => codeVisible.value = true
-    const hideCode = () => codeVisible.value = false
+    const showCode = () => (codeVisible.value = true)
+    const hideCode = () => (codeVisible.value = false)
     const codeVisible = ref(false)
     return {
       Prism,
       html,
       codeVisible,
       showCode,
-      hideCode
+      hideCode,
     }
-  }
+  },
 }
 </script>
+
+<style lang="scss">
+@import 'prismjs/themes/prism.css';
+</style>
 
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
@@ -56,7 +60,7 @@ $border-color: #d9d9d9;
   border: 1px solid $border-color;
   margin: 16px 0 32px;
 
-  >h2 {
+  > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
@@ -75,7 +79,7 @@ $border-color: #d9d9d9;
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
 
-    >pre {
+    > pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;

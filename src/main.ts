6c9b6fe47cc3c43bb89cfe04338acea8fcb9b0a3
Di Xiao <mopjtv@gmail.com>
2021-08-30 21:49:36 +0800

    update wave ui

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import './assets/svg.js'
+// import './assets/scss-icon.js'
+
 import 'github-markdown-css'
 import Markdown from './components/Markdown.vue'
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,40 +3,88 @@
     <div class="topnavAndBanner">
       <Topnav />
       <div class="banner">
-        <h1>VueXD UI</h1>
-        <h2>一个面向学习者的 UI 框架</h2>
+        <h1>造轮子</h1>
+        <h3>一个自建的 Vue 3 框架</h3>
         <p class="actions">
           <a href="https://github.com/deexiao/VueXD">GitHub</a>
           <router-link to="/doc">开始</router-link>
         </p>
+      </div>
+      <div>
+        <svg
+          class="waves"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28"
+          preserveAspectRatio="none"
+          shape-rendering="auto"
+        >
+          <defs>
+            <path
+              id="gentle-wave"
+              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+            />
+          </defs>
+          <g class="parallax">
+            <use
+              xlink:href="#gentle-wave"
+              x="48"
+              y="0"
+              fill="rgba(255,255,255,0.7"
+            />
+            <use
+              xlink:href="#gentle-wave"
+              x="48"
+              y="3"
+              fill="rgba(255,255,255,0.5)"
+            />
+            <use
+              xlink:href="#gentle-wave"
+              x="48"
+              y="5"
+              fill="rgba(255,255,255,0.3)"
+            />
+            <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+          </g>
+        </svg>
       </div>
     </div>
     <div class="features">
       <ul>
         <li>
           <svg>
-            <use xlink:href="#icon-vue"></use>
+            <use xlink:href="#icon-vuex"></use>
           </svg>
-          <h3>基于 Vue 3</h3>
-          <p>骄傲地使用了 Vue 3 Composition API</p>
+          <a>基于 Vue 3</a>
+          <p>使用了 Composition API</p>
         </li>
         <li>
           <svg>
             <use xlink:href="#icon-ts"></use>
           </svg>
-          <h3>基于 TypeScript</h3>
+          <a>基于 TypeScript</a>
           <p>源代码采用 TypeScript 书写</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-scss"></use>
+          </svg>
+          <a>基于 SCSS</a>
+          <p>开发效率更高且易维护</p>
         </li>
         <li>
           <svg>
             <use xlink:href="#icon-light"></use>
           </svg>
-          <h3>代码易读</h3>
-          <p>每个组件的源代码都极其简洁</p>
+          <a>代码易读</a>
+          <p>模块化的源代码简单明了</p>
         </li>
       </ul>
     </div>
   </div>
+  <!-- <div class="content flex">
+    <p>deexiao.com - 2021 - 造轮子</p>
+  </div> -->
 </template>
 
 <script lang="ts">
@@ -48,24 +96,144 @@ export default {
 }
 </script>
 
+<style scoped>
+@import url(//fonts.googleapis.com/css?family=Lato:300:400);
+
+body {
+  margin: 0;
+}
+
+h1 {
+  font-family: 'Lato', sans-serif;
+  font-weight: 300;
+  letter-spacing: 2px;
+  font-size: 48px;
+}
+p {
+  letter-spacing: 1px;
+  font-size: 14px;
+  color: #333333;
+}
+
+.header {
+  position: relative;
+  text-align: center;
+  background: linear-gradient(
+    60deg,
+    rgba(84, 58, 183, 1) 0%,
+    rgba(0, 172, 193, 1) 100%
+  );
+  color: white;
+}
+.logo {
+  width: 50px;
+  fill: white;
+  padding-right: 15px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.inner-header {
+  height: 65vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.flex {
+  /*Flexbox for containers*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.waves {
+  position: relative;
+  width: 100%;
+  height: 15vh;
+  margin-bottom: -7px; /*Fix for safari gap*/
+  min-height: 100px;
+  max-height: 150px;
+}
+
+.content {
+  position: relative;
+  height: 20vh;
+  text-align: center;
+  background-color: white;
+}
+
+/* Animation */
+
+.parallax > use {
+  animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
+  0% {
+    transform: translate3d(-90px, 0, 0);
+  }
+  100% {
+    transform: translate3d(85px, 0, 0);
+  }
+}
+/*Shrinking for mobile*/
+@media (max-width: 768px) {
+  .waves {
+    height: 40px;
+    min-height: 40px;
+  }
+  .content {
+    height: 30vh;
+  }
+  h1 {
+    font-size: 32px;
+  }
+}
+</style>
+
 <style lang="scss" scoped>
-$green: #02bcb0;
-$border-radius: 4px;
-$color: #007974;
+$green: #8a96dd;
+$border-radius: 20px;
+$color: #4e5875;
 
 .topnavAndBanner {
   background: linear-gradient(
-    145deg,
-    rgba(227, 255, 253, 1) 0%,
-    rgba(183, 233, 230, 1) 100%
+    156deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(183, 205, 255, 1) 41%,
+    rgba(151, 128, 255, 1) 100%
   );
-  clip-path: ellipse(80% 60% at 50% 40%);
+
+  // clip-path: ellipse(80% 60% at 50% 40%);
 }
 
 .features {
   margin: 64px auto;
   padding: 0 16px;
-
+  a {
+    font-size: 2ch;
+  }
+  p {
+    font-size: 1.5ch;
+    color: #61646e;
+  }
   @media (min-width: 800px) {
     width: 800px;
 
@@ -76,12 +244,12 @@ $color: #007974;
     }
   }
 
-  @media (min-width: 1200px) {
-    width: 1200px;
+  @media (min-width: 1400px) {
+    width: 1400px;
 
     > ul {
       > li {
-        width: 33.3333%;
+        width: 25%;
       }
     }
   }
@@ -103,8 +271,8 @@ $color: #007974;
 
       > svg {
         grid-area: icon;
-        width: 64px;
-        height: 64px;
+        width: 54px;
+        height: 54px;
       }
 
       > h3 {
@@ -129,11 +297,10 @@ $color: #007974;
 
   > .actions {
     padding: 8px 0;
-
     a {
       margin: 0 8px;
       background: $green;
-      color: white;
+      color: #faefef;
       display: inline-block;
       padding: 8px 24px;
       border-radius: $border-radius;
